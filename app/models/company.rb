1165e61a7787f160b9b58306ee95a8aad37c0f39
Shubham Rajput <shubham.r@simformsolutions.com>
2023-03-09 16:34:20 +0530

    associate user and company

---
+++ app/models/company.rb
@@ -0,0 +1,4 @@
+class Company < ApplicationRecord
+  has_many :memberships
+  has_many :users, through: :memberships
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable, :lockable, :timeoutable, :trackable
 
   validates :first_name, :last_name, presence: true
+
+  has_many :memberships
+  has_many :companies, through: :memberships
 end
